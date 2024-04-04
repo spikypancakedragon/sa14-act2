d06e1ae1e9acd4afe3986df3f5786ad38db5cfcd
Avery Walker <spikypancakedragon@gmail.com>
2024-04-04 16:26:24 -0500

    added 5 day forecast

--- prob1/prob1.js
+++ prob1/prob1.js
@@ -4,13 +4,9 @@ form.addEventListener('submit',
 async function(event) { 
     event.preventDefault();
     var cityName = document.getElementById('city').value;
-    var thisThing = "http://api.weatherapi.com/v1/current.json?key=b1f7372db07c43cfbee23040242803&q=" + cityName
+    var thisThing = "http://api.weatherapi.com/v1/forecast.json?key=b1f7372db07c43cfbee23040242803&q=" + cityName + "&days=4&aqi=no&alerts=no"
     var response = await fetch(thisThing)
     var json = await response.json();
-
-    // var paragraph = document.createElement('p');
-    // paragraph.textContent = JSON.stringify(json);
-    // document.body.appendChild(paragraph);
 
     var divs = document.getElementsByClassName("display");
     for (var i = 0; i < divs.length; i++) {
@@ -34,5 +30,25 @@ async function(event) {
 
     var icon = document.getElementById("icon");
     icon.src = "https:" + json["current"]["condition"]["icon"]
+console.log("forecast", json)
+    document.getElementById("high1").textContent = json["forecast"]["forecastday"][0]["day"]["maxtemp_f"]
+    document.getElementById("low1").textContent = json["forecast"]["forecastday"][0]["day"]["mintemp_f"]
+    document.getElementById("cond1").textContent = json["forecast"]["forecastday"][0]["day"]["condition"]["text"]
+    document.getElementById("forecast1").src = "https:" + json["forecast"]["forecastday"][0]["day"]["condition"]["icon"]
+
+    document.getElementById("high2").textContent = json["forecast"]["forecastday"][1]["day"]["maxtemp_f"]
+    document.getElementById("low2").textContent = json["forecast"]["forecastday"][1]["day"]["mintemp_f"]
+    document.getElementById("cond2").textContent = json["forecast"]["forecastday"][1]["day"]["condition"]["text"]
+    document.getElementById("forecast2").src = "https:" + json["forecast"]["forecastday"][1]["day"]["condition"]["icon"]
+
+    document.getElementById("high3").textContent = json["forecast"]["forecastday"][2]["day"]["maxtemp_f"]
+    document.getElementById("low3").textContent = json["forecast"]["forecastday"][2]["day"]["mintemp_f"]
+    document.getElementById("cond3").textContent = json["forecast"]["forecastday"][2]["day"]["condition"]["text"]
+    document.getElementById("forecast3").src = "https:" + json["forecast"]["forecastday"][2]["day"]["condition"]["icon"]
+
+    document.getElementById("high4").textContent = json["forecast"]["forecastday"][3]["day"]["maxtemp_f"]
+    document.getElementById("low4").textContent = json["forecast"]["forecastday"][3]["day"]["mintemp_f"]
+    document.getElementById("cond4").textContent = json["forecast"]["forecastday"][3]["day"]["condition"]["text"]
+    document.getElementById("forecast4").src = "https:" + json["forecast"]["forecastday"][3]["day"]["condition"]["icon"]
 });
 
